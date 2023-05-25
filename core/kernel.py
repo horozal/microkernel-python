10b3c3096e5beaf8af96f7906031c31b13103dd7
horozal <36187605+horozal@users.noreply.github.com>
2023-05-25 18:29:44 +0200

    Update kernel.py

--- core/kernel.py
+++ core/kernel.py
@@ -1,34 +1,7 @@
 class Microkernel:
     def __init__(self):
-        self.plugins = []
+        pass 
+    
+kernel = Microkernel
 
-    def load_plugin(self, plugin):
-        self.plugins.append(plugin)
-
-    def run(self):
-        print("Microkernel is running.")
-
-        for plugin in self.plugins:
-            plugin.execute()
-
-
-class Plugin:
-    def __init__(self, name):
-        self.name = name
-
-    def execute(self):
-        print(f"Executing plugin: {self.name}")
-
-
-if __name__ == "__main__":
-    kernel = Microkernel()
-
-    # Load plugins
-    plugin = Plugin("Plugin")
-)
-
-    kernel.load_plugin(plugin)
-
-  
-    # Run the microkernel
-    kernel.run()
+print(kernel)
